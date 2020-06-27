e5378d41e60d586988bba7d685abe78369621b85
Mondanzo <hamburg.karl.nehring@gmail.com>
2020-06-27 03:51:26 +0200

    Extracting meta now works

--- public/views/CMod.vue
+++ public/views/CMod.vue
@@ -4,8 +4,9 @@
     class="rounded shadow-lg max-w-lg w-full p-8 m-12 bg-blue-600 text-white"
   >
     <h1 class="text-3xl">Upload Mod File</h1>
-    <input type="file" />
+    <input type="file" @change="filesChange($event.target.files)" />
     <form class="flex flex-col items-center h-100 w-100">
+      <h1 class="text-2xl">Mod Info</h1>
       <label for="st-key">Mod Slug</label>
       <input
         class="rounded border-gray-500 border"
@@ -46,6 +47,16 @@
         <label for="st-type"></label>
       </div>
     </form>
+    <form class="flex flex-col items-center h-100 w-100">
+      <h1 class="text-2xl">Mod Build</h1>
+      <label for="st-version">Version</label>
+      <input
+        class="rounded border-gray-500 border"
+        id="st-version"
+        name="st-version"
+        v-model="modversion"
+      />
+    </form>
   </div>
 </template>
 <script>
@@ -61,31 +72,35 @@ export default {
       website: "",
       side: null,
       curse: null,
+      modversion: null,
     };
   },
   methods: {
-    loadMeta: function(meta) {
+    loadMeta(meta) {
       console.log(meta.modid);
       this.key = meta.modid || "";
       this.name = meta.name || "";
       this.author = meta.authorList ? meta.authorList[0] : "";
       this.desc = meta.description || "";
-      this.website = meta.website || "";
+      this.website = meta.url || "";
+      this.modversion = (meta.mcversion + "-" || "") + (meta.version || "");
     },
-    filesChange: async function(files) {
-      let zip = await jszip.loadAsync(files[0]);
-      let mcmod = zip.file("mcmod.info");
-      if (mcmod) {
-        let content = await mcmod.async("text");
-        let json = JSON.parse(content);
-        if (Array.isArray(json)) {
-          this.loadMeta(json[0]);
+    filesChange(files) {
+      jszip.loadAsync(files[0]).then((zip) => {
+        let mcmod = zip.file("mcmod.info");
+        if (mcmod) {
+          mcmod.async("text").then((content) => {
+            let json = JSON.parse(content);
+            if (Array.isArray(json)) {
+              this.loadMeta(json[0]);
+            } else {
+              this.loadMeta(json);
+            }
+          });
         } else {
-          this.loadMeta(json);
+          this.$vToastify.error("Couldn't find a mcmod.info file!");
         }
-      } else {
-        this.$vToastify.error("Couldn't find a mcmod.info file!");
-      }
+      });
     },
   },
 };
@@ -97,6 +112,7 @@ textarea {
   padding: 2px 4px;
   width: 100%;
   margin-bottom: 12px;
+  color: #000;
 }
 
 form,
